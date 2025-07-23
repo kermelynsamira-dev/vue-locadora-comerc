d683281b7bbf65337ee52e7e06c09177ef8e6e8c
kermelynsamira-dev <work.ksamira@gmail.com>
2025-07-23 00:11:20 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="p-4 text-center">
+    <h1 class="text-3xl font-bold text-blue-500">Tailwind funcionando! 🚀</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Você pode deixar vazio por enquanto
+</script>
+
+<style scoped>
+/* Pode deixar vazio ou remover */
+</style>
